f412fb6ede6adc2a298efa82c52f4df1cf122b59
Vinoth Ravindran <vinoth17@hotmail.fr>
2019-02-03 23:35:43 +0100

    delete.php(avec coms)

--- monsters_league_poo/delete.php
+++ monsters_league_poo/delete.php
@@ -7,43 +7,32 @@ $monsters = getMonsters();
 
 
 
-/*if (is_null($monster)) {
-    header('Location: /index.php?error=missing_data');
-    die;
-}
-*/
-
-if (!isset($_POST['monster'])) {
-    header('Location: /index.php?error=bad_ships');
+if (!isset($_POST['monster'])) { //permet de savoir si la variable monster est définie
+    header('Location: /index.php?error=bad_ships'); // renvoyer vers la page indiquée
     die;
 }
 
 
-$monstertemp=$_POST['monster'];
+$monstertemp=$_POST['monster']; 
 echo($monstertemp);
 
-if (is_null($monstertemp)) {
-    header('Location: /index.php?error=missing_data');
+if (is_null($monstertemp)) { //si la variable monstertemp vaut null
+    header('Location: /index.php?error=missing_data'); //renvoyer vers la page indiquée 
     die;
 }
 
 
 try{
-	$db = new PDO('mysql:host=localhost;dbname=monstres', 'root', '0000');
+	$db = new PDO('mysql:host=localhost;dbname=monstres', 'root', '0000'); //connexion a notre bdd
 }
-catch(Exception $e){
+catch(Exception $e){ // on lance une exception si on arrive pas a se connecter
 	die("Failed to connect to database".$e->getMessage());
 }
 
-$myPDO=$db->query("DELETE FROM `mymonsters` WHERE  `mymonsters`.`name`='".$monstertemp."'");
+$myPDO=$db->query("DELETE FROM `mymonsters` WHERE  `mymonsters`.`name`='".$monstertemp."'");//requete sql pour supprimer un monstre de notre bdd
 
-//if(is_null($myPDO)) echo("erreur");
-	
-//$myPDO->bindValue(':name', $monster);
 
-//if($result= $myPDO->execute()) echo("Monstre supprimé");
-
-header('Location:index.php');
+header('Location:index.php'); 
 die;
 
 ?>
